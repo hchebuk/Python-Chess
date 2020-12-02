2b9dad706fca5ead9712347895215b5cf4ecc302
Kenny Nguyen <72421222+itsmeKenny@users.noreply.github.com>
2020-12-01 21:58:48 -0800

    Add files via upload

--- PythonChessTkinter.py
+++ PythonChessTkinter.py
@@ -1,8 +1,11 @@
 #Author: Kenny Nguyen
 #Project: Chess in python for CIS 407
 #Description: This file will be the start menu for the chess game.
+#Version 1.1
 
 import tkinter as tk
+import PythonChessBoard
+import PythonChessMain
 
 myheight = 600
 mywidth = 400
@@ -23,13 +26,13 @@ frame.place(relx=0.11, rely=0.1, relwidth=0.8, relheight=0.8)
 title = tk.Label(frame, text="Python Chess!", bg="pink")
 title.place(relx=0.4 ,rely='0')
 
-oneplayerbutton = tk.Button(frame, text="Single Player", bg='black', fg='white')
+oneplayerbutton = tk.Button(frame, text="Single Player", bg='black', fg='white', command = PythonChessMain.main() )
 oneplayerbutton.place(relx=0.40, rely=0.3)
 
-twoplayerbutton = tk.Button(frame, text="Player vs Player", bg='black', fg='white')
+twoplayerbutton = tk.Button(frame, text="Player vs Player", bg='black', fg='white', command = PythonChessMain.main() )
 twoplayerbutton.place(relx=0.4, rely=0.5)
 
-exitbutton = tk.Button(frame, text="Exit Game", bg='black', fg='white')
+exitbutton = tk.Button(frame, text="Exit Game", bg='black', fg='white', command = quit)
 exitbutton.place(relx=0.40, rely=0.7)
 
 root.mainloop()
